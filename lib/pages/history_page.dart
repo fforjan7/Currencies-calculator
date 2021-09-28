00a6c1cba87498c0056472500c4d8259efc039dc
Filip Forjan <filip.forjan7@gmail.com>
2021-09-28 13:41:59 +0200

    History page UI finished

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -33,19 +33,117 @@ class HistoryPage extends StatelessWidget {
             return ListView.separated(
               itemBuilder: (context, index) => Row(
                 children: [
-                  Text(
-                    (DateFormat('dd-MM-yyyy')
-                        .format(DateTime.parse(state.savedData[index]['date']))
-                        .toString()),
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                  Text(
-                    state.savedData[index]['currency1Name'],
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                  Text(
-                    state.savedData[index]['currency1Value'],
-                    style: TextStyle(color: Colors.blue),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: Text(
+                            (DateFormat('dd-MM-yyyy')
+                                .format(DateTime.parse(
+                                    state.savedData[index]['date']))
+                                .toString()),
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 12.0),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(10.0),
+                              height: MediaQuery.of(context).size.height * 0.12,
+                              width: MediaQuery.of(context).size.width * 0.25,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.06,
+                                    width: MediaQuery.of(context).size.width *
+                                        0.25,
+                                    child: FittedBox(
+                                      child: Text(
+                                        state.savedData[index]
+                                            ['currency1Value'],
+                                        style: TextStyle(
+                                            color: Colors.red, fontSize: 35),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    state.savedData[index]['currency1Name'],
+                                    style: TextStyle(
+                                        color: Colors.blue, fontSize: 15),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_right_alt_rounded,
+                              color: Colors.green,
+                              size: 60.0,
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(10.0),
+                              height: MediaQuery.of(context).size.height * 0.12,
+                              width: MediaQuery.of(context).size.width * 0.25,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.06,
+                                    width: MediaQuery.of(context).size.width *
+                                        0.25,
+                                    child: FittedBox(
+                                      child: Text(
+                                        state.savedData[index]
+                                            ['currency2Value'],
+                                        style: TextStyle(
+                                            color: Colors.blue, fontSize: 35),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    state.savedData[index]['currency2Name'],
+                                    style: TextStyle(
+                                        color: Colors.blue, fontSize: 15),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(10.0),
+                              height: MediaQuery.of(context).size.height * 0.12,
+                              width: MediaQuery.of(context).size.width * 0.25,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.06,
+                                    width: MediaQuery.of(context).size.width *
+                                        0.25,
+                                    child: FittedBox(
+                                      child: Text(
+                                        state.savedData[index]
+                                            ['currency3Value'],
+                                        style: TextStyle(
+                                            color: Colors.blue, fontSize: 35),
+                                      ),
+                                    ),
+                                  ),
+                                  Text(
+                                    state.savedData[index]['currency3Name'],
+                                    style: TextStyle(
+                                        color: Colors.blue, fontSize: 15),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
